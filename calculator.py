38547ea912cc35d79fb902f47164726069037ee7
PATEL242620 <kp8842015@example.com>
2026-06-18 21:52:19 +0530

    feat: counter with calculator

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def add(n, m):
+    return n + m;
+
+sumOftownumbers = add(5,4)
+
+print(sumOftownumbers)
---
+++ counter.py
@@ -0,0 +1,10 @@
+counter = 1
+
+def incrementCount(): 
+    counter += 1
+
+def decremntCount():
+    if(not counter > 0):
+        counter -= 1
+
+    return counter
